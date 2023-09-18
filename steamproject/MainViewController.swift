105a33aac6bb56fd60a4271d148c48fcff52ce6c
ahnjaewon <ajw0376@gmail.com>
2023-09-18 16:49:05 +0900

    2023.09.18
    최초 Tabbar 구성된 것.

---
+++ steamproject/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  steamproject
+//
+//  Created by AHNJAEWON1 on 2023/09/18.
+//
+// 2023.09.28 MainPage 생성
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ steamproject/view/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  steamproject
+//
+//  Created by AHNJAEWON1 on 2023/09/18.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // 로그인 이미지
+    @IBOutlet weak var imgView: UIImageView!
+    
+    var imageName = "loginView.png"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+     let image = UIImage(named: imageName)
+        imgView.image = image
+    }
+    
+
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+} // ViewController
